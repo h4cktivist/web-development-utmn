e0d18d1663a6c864f54689718afef206b657f92d
h4cktivist <popovvadim0605@gmail.com>
2024-11-14 10:10:03 +0500

    fix: page names

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Футбольные клубы</title>
+    <title>Добавить ФК</title>
     @vite(['resources/sass/styles.sass', 'resources/js/index.js'])
 </head>
 <body>
